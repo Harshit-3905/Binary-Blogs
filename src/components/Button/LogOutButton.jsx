fa210d62bca2e83956c05b767695fa41029ef11c
Harshit-3905 <harshitjoshileet@gmail.com>
2024-01-04 00:39:16 +0530

    Add LogOutButton and LoginButton components

---
+++ src/components/Button/LogOutButton.jsx
@@ -0,0 +1,9 @@
+const LogOutButton = () => {
+  return (
+    <button type="button" className="w-[50px] h-[40px] bg-red-500">
+      LogOut
+    </button>
+  );
+};
+
+export default LogOutButton;
---
+++ src/components/Button/LoginButton.jsx
@@ -0,0 +1,9 @@
+const LoginButton = () => {
+  return (
+    <button type="button" className="w-[50px] h-[40px] bg-red-500">
+      Login/SignUp
+    </button>
+  );
+};
+
+export default LoginButton;
