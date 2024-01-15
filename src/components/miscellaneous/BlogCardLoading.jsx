e43a9fcc8ac6f6ad351ba05757bb2611966bd716
Harshit-3905 <harshitjoshileet@gmail.com>
2024-01-15 21:39:43 +0530

    Refactor BlogCardLoading component

--- src/components/miscellaneous/BlogCardLoading.jsx
+++ src/components/miscellaneous/BlogCardLoading.jsx
@@ -4,9 +4,7 @@ const BlogCard = () => {
   return (
     <div className="w-[300px] h-[380px] bg-[#00B4D8] rounded-3xl flex justify-center items-center text-center">
       <div className=" w-[full] h-[full] flex flex-col">
-        <div className="h-[150px] w-full">
-          <img src={defaultImage} className="w-full h-full rounded-2xl" />
-        </div>
+        <img src={defaultImage} className="h-[150px] w-full rounded-2xl" />
         <div className="w-full h-[2rem] mt-3 font-medium bg-slate-400 rounded-2xl"></div>
         <div className="w-full h-[100px] mt-3 overflow-hidden bg-slate-400 rounded-2xl"></div>
       </div>
