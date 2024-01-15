e040e6a4152e8da5c72748228c393718e665e950
Harshit-3905 <harshitjoshileet@gmail.com>
2024-01-15 21:45:48 +0530

    Add author name and link to footer

--- src/components/Common/Footer.jsx
+++ src/components/Common/Footer.jsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 const Footer = () => {
   return (
     <div className="w-full h-[5vh] text-black bg-[#48CAE4] flex items-center justify-center px-5">
-      <div className="text-md">&copy; 2024 . All Rigths Reserved</div>
+      <p className="text-md">
+        &copy; 2024 . All Rigths Reserved. Made by{" "}
+        <Link
+          to="https://www.linkedin.com/in/harshit-joshi-40953321b/"
+          className="text-blue-950 font-bold"
+          target="_blank"
+        >
+          Harshit Joshi
+        </Link>
+      </p>
     </div>
   );
 };
