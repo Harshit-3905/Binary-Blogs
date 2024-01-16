dbb2e877a1cca26b567ab7a742c4359a0d36b998
Harshit-3905 <harshitjoshileet@gmail.com>
2024-01-16 13:16:26 +0530

    Update Footer and Header components

--- src/components/Common/Header.jsx
+++ src/components/Common/Header.jsx
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/authSlice";
 import logo from "../../assets/Logo.png";
+import { ToastContainer, toast } from "react-toastify";
 
 const Header = () => {
   const navigate = useNavigate();
@@ -12,6 +13,7 @@ const Header = () => {
   const authStatus = useSelector((state) => state.auth.status);
   const LogoutHandler = () => {
     authService.logout();
+    toast.success("Logout Successful");
     dispatch(logout());
     navigate("/");
   };
@@ -30,50 +32,64 @@ const Header = () => {
     },
   ];
   return (
-    <div className="w-full h-[10vh] bg-[#48CAE4] flex items-center justify-between px-10">
-      <div className="h-full p-2">
-        <Link to="/">
-          <img
-            src={logo}
-            alt="Binary Blogs"
-            className="h-full bg-transparent"
-          />
-        </Link>
-      </div>
-      <div className="w-[400px]">
-        <ul className="flex justify-evenly">
-          {NavItems.map((item) => (
-            <NavLink
-              key={item.name}
-              to={item.link}
-              className={({ isActive }) =>
-                `block py-2 pr-4 pl-3 duration-200 ${
-                  isActive
-                    ? "underline underline-offset-4 text-[#03045E]"
-                    : "no-underline"
-                } text-xl hover:text-[#03045E] font-medium`
-              }
-            >
-              {item.name}
-            </NavLink>
-          ))}
-        </ul>
-      </div>
-      {authStatus ? (
-        <button
-          className="h-12 w-32 bg-red-600 p-3 rounded-xl text-white"
-          onClick={LogoutHandler}
-        >
-          LogOut
-        </button>
-      ) : (
-        <Link to="/auth/login">
-          <button className="h-12 w-32 bg-red-600 p-3 rounded-xl text-white">
-            Login/SignUp
+    <>
+      <div className="w-[100vw] h-[10vh] bg-[#48CAE4] flex items-center justify-between px-10">
+        <div className="h-full p-2">
+          <Link to="/">
+            <img
+              src={logo}
+              alt="Binary Blogs"
+              className="h-full bg-transparent"
+            />
+          </Link>
+        </div>
+        <div className="w-[400px]">
+          <ul className="flex justify-evenly">
+            {NavItems.map((item) => (
+              <NavLink
+                key={item.name}
+                to={item.link}
+                className={({ isActive }) =>
+                  `block py-2 pr-4 pl-3 duration-200 ${
+                    isActive
+                      ? "underline underline-offset-4 text-[#03045E]"
+                      : "no-underline"
+                  } text-xl hover:text-[#03045E] font-medium`
+                }
+              >
+                {item.name}
+              </NavLink>
+            ))}
+          </ul>
+        </div>
+        {authStatus ? (
+          <button
+            className="h-12 w-32 bg-red-600 p-3 rounded-xl text-white"
+            onClick={LogoutHandler}
+          >
+            LogOut
           </button>
-        </Link>
-      )}
-    </div>
+        ) : (
+          <Link to="/auth/login">
+            <button className="h-12 w-32 bg-red-600 p-3 rounded-xl text-white">
+              Login/SignUp
+            </button>
+          </Link>
+        )}
+      </div>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover={false}
+        theme="light"
+      />
+    </>
   );
 };
 
