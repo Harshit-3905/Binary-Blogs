b475ec1f5e84eacea64ae509a887fb80c472ae0f
Harshit-3905 <harshitjoshileet@gmail.com>
2024-01-25 21:36:31 +0530

    Add authentication status check in LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -2,8 +2,11 @@ import { Outlet } from "react-router-dom";
 import image from "../assets/auth.webp";
 import { Suspense } from "react";
 import LoadingPage from "./LoadingPage";
+import { useSelector } from "react-redux";
 
 const LoginPage = () => {
+  const authStatus = useSelector((state) => state.auth.status);
+  if (authStatus) return <p className="text-2xl">You are Already Logged In</p>;
   return (
     <div className="w-full min-h-[50vh] md:h-full flex rounded-3xl flex-col lg:flex-row">
       <div className="w-full h-[50%] flex items-center justify-center">
