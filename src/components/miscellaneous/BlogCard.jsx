5959d386f6ba31de71bf7f5e1698428e8ba87205
Harshit Joshi <harshitjoshileet@gmail.com>
2024-09-24 13:06:34 +0530

    Refactor BlogCard component and add navigation to individual blog page

--- src/components/miscellaneous/BlogCard.jsx
+++ src/components/miscellaneous/BlogCard.jsx
@@ -2,15 +2,26 @@ import PropTypes from "prop-types";
 import parse from "html-react-parser";
 import LikeContainer from "../miscellaneous/Like.jsx";
 import { EyeIcon } from "@primer/octicons-react";
+import { useNavigate } from "react-router-dom";
 
 const BlogCard = (props) => {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate(`/blog/${props.id}`);
+  };
+
   return (
-    <div className="w-[300px] h-[390px] bg-[#00B4D8] rounded-3xl flex justify-center items-center text-center">
+    <div
+      className="w-[300px] h-[390px] bg-[#00B4D8] rounded-3xl flex justify-center items-center text-center cursor-pointer"
+      onClick={handleClick}
+    >
       <div className=" w-[full] h-[full] p-5 flex flex-col">
         <img
           src={props.image}
           className="h-[150px] w-full rounded-2xl"
           loading="lazy"
+          alt={props.title}
         />
         <h1 className="w-full text-2xl mt-3 font-medium overflow-hidden">
           {props.title}
@@ -28,6 +39,7 @@ const BlogCard = (props) => {
 };
 
 BlogCard.propTypes = {
+  id: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
   content: PropTypes.string.isRequired,
   image: PropTypes.object.isRequired,
